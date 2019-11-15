70717aa08c75b39e8158ed1ca58ef0d0fe08d445
DallasLind <mcgraw.dallas@gmail.com>
2019-11-15 15:42:28 -0500

    Styling with bootstrap

--- find-that-dmv-brewery-app/src/components/breweries.js
+++ find-that-dmv-brewery-app/src/components/breweries.js
@@ -3,16 +3,16 @@ import React from 'react'
     const Breweries= ({ breweries }) => {
       return (
         <div>
-          <center><h1>DMV Breweries! </h1></center>
+          <p class="h1 text-center bg-dark text-white">DMV Breweries</p>
           {breweries.map((brewery) => (
             <div className="card">
               <div className="card-body">
                 <h3 className="card-title">{brewery.name}</h3>
                 <h4 className="card-subtitle mb-2 text-muted">{brewery.breweryType}</h4>
-                <p className="card-text">
+                <p className="card-text text-center">
                 {brewery.state} 
                 </p>
-                <p className="card-text">
+                <p className="card-text text-center">
                 {brewery.city}
                 </p>
               </div>
