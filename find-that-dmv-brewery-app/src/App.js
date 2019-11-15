59e0aa8818f922f887ecf77934113305ed00c5a4
DallasLind <mcgraw.dallas@gmail.com>
2019-11-15 09:50:27 -0500

    Starting construction of App

--- find-that-dmv-brewery-app/src/App.js
+++ find-that-dmv-brewery-app/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Home from './Home.js';
+import breweries from './data/breweries.json';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      brewery: null
+    }
+    this.setBrewery = this.setBrewery.bind(this);
+  }
+  setBrewery(brewery){
+    this.setState({brewery: brewery})
+  }
+render(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav> </Nav>
+        <Route path="/" exact component = {Home}></Route>
+        <Route path="/breweryinfo" exact component = {BreweryInfo}></Route>
+  <Route exact path= "/breweryinfo:brewery" render = {routerProps => <BreweryInfo setBrewery = {this.setBrewery}{...routerProps}/>}{...this.state}></Route>
     </div>
   );
 }
-
+}
 export default App;
